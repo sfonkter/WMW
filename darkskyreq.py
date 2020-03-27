0a4522ca1972c411d874aadd4b97c390b9a8db78
delaney <dck5216@psu.edu>
2020-03-27 01:16:07 -0400

    fixed env vars

--- darkskyreq.py
+++ darkskyreq.py
@@ -9,9 +9,7 @@ class Weather:
     
     def getCoords(self):
         google_key = os.environ['GOOGLE_API_KEY']
-        print (google_key)
-        response = requests.get("https://maps.googleapis.com/maps/api/geocode/json?address=%s&key=%s"%(self.loc.replace(" ","+")), google_key)
-        print(response)
+        response = requests.get("https://maps.googleapis.com/maps/api/geocode/json?address=%s&key=%s"%(self.loc.replace(" ","+"), google_key))
         try:
             coords = response.json()['results'][0]
             return coords
