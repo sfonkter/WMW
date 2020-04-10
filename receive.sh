64256b4b4ef88f43402f0c8640edc160611e5ceb
delaney <dck5216@psu.edu>
2020-04-10 14:07:26 -0400

    fixed receive.sh so it doesn't use ngrok

--- receive.sh
+++ receive.sh
@@ -1,5 +1,5 @@
 workon
 cd ~/WMW
-twilio phone-numbers:update "+18647546178" --sms-url="http://localhost:5000/sms" &
+twilio phone-numbers:update "+18647546178" --sms-url="http://192.241.149.241:5000/sms" &
 python run.py &
 python scheduler.py &
