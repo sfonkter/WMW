481093f74355b551b0fc4206ea9c839fb50faa61
delaney <dck5216@psu.edu>
2020-04-11 19:34:46 -0400

    pointed to correct image folder

--- MMSimage.py
+++ MMSimage.py
@@ -15,8 +15,4 @@ def img(customer_id):
     draw.text(xy=(80, 80), text="\n".join(textwrap.wrap(message, width=35)), fill=(0, 0, 0))
     image.save('Weather-Photos/updates/user{}.jpg'.format(customer_id))
 
-    return 'http://192.241.149.241:8000/updates/user{}.jpg'.format(customer_id)
-
-
-if __name__ == "__main__":
-    img(8)
+    return 'http://192.241.149.241:8000/Weather-Photos/updates/user{}.jpg'.format(customer_id)
