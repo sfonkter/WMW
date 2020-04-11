23c53f8fabec8965e17bc220c27e25cae4d9093f
delaney <dck5216@psu.edu>
2020-04-11 19:25:02 -0400

    fixing fonts for live

--- MMSimage.py
+++ MMSimage.py
@@ -7,12 +7,12 @@ def img(customer_id):
     message, icon = msg.msg(customer_id)
     icon = 'clear-day'
 
-    font = ImageFont.truetype(r'C:\WINDOWS\Fonts\CALIBRI.TTF', 60)
+    #font = ImageFont.truetype('CALIBRI.TTF', 60)
 
     image = Image.open('Weather-Photos/templates/{}.jpg'.format(icon))
 
     draw = ImageDraw.Draw(image)
-    draw.text(xy=(80, 80), text="\n".join(textwrap.wrap(message, width=35)), fill=(0, 0, 0), font=font)
+    draw.text(xy=(80, 80), text="\n".join(textwrap.wrap(message, width=35)), fill=(0, 0, 0))
     image.save('Weather-Photos/updates/user{}.jpg'.format(customer_id))
 
     return 'http://192.241.149.241:8000/updates/user{}.jpg'.format(customer_id)
