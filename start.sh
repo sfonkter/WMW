106ffe00ebd0439f735584709dffa0ec36318c72
delaney <dck5216@psu.edu>
2020-04-14 16:07:53 -0400

    fixed where MMSimage.py points for saved images and added http server to start.sh

--- start.sh
+++ start.sh
@@ -3,3 +3,5 @@ cd ~/WMW
 twilio phone-numbers:update "+18647546178" --sms-url="http://192.241.149.241:5000/sms" &
 python run.py &
 python scheduler.py &
+cd ~/WMW/Weather-Photos
+python -m http.server 8000
