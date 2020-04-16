acf10e426dca5e0b9f2059f6728bcd985b466a81
delaney <dck5216@psu.edu>
2020-04-16 16:32:52 -0400

    Made text start lower to better suite ipones

--- MMSimage.py
+++ MMSimage.py
@@ -12,9 +12,9 @@ def img(customer_id):
     font = ImageFont.truetype(r'{}'.format(font_folder), 50)
 
     image = Image.open('Weather-Photos/templates/{}.jpg'.format(icon))
-    # todo make the image smaller so it fits on the preview on phones
+
     draw = ImageDraw.Draw(image)
-    draw.text(xy=(70, 230), text="\n".join(textwrap.wrap(message, width=35)), fill=(0, 0, 0), font=font)
+    draw.text(xy=(70, 270), text="\n".join(textwrap.wrap(message, width=35)), fill=(0, 0, 0), font=font)
     image.save('Weather-Photos/user{}.jpg'.format(customer_id))
 
     return 'http://192.241.149.241:8000/user{}.jpg'.format(customer_id)
