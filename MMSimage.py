ec55db077c911151f4382b5cb51cfe5d0e115a99
delaney <dck5216@psu.edu>
2020-04-17 13:16:00 -0400

    added a default template

--- MMSimage.py
+++ MMSimage.py
@@ -7,14 +7,16 @@ import os
 def img(customer_id):
     message, icon = msg.msg(customer_id)
     font_folder = os.environ['FONT_FOLDER']
+    # set default icon
+    default_icon = 'clear-day'
 
     font = ImageFont.truetype(r'{}'.format(font_folder), 50)
-
+    print(icon)
     try:
         image = Image.open('Weather-Photos/templates/{}.jpg'.format(icon))
     except Exception as e:
         print(e)
-        image = Image.open('Weather-Photos/templates/clear-day.jpg')
+        image = Image.open('Weather-Photos/templates/{}.jpg'.format(default_icon))
 
     draw = ImageDraw.Draw(image)
     draw.text(xy=(70, 270), text="\n".join(textwrap.wrap(message, width=35)), fill=(0, 0, 0), font=font)
