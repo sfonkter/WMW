a38715687a164a7d686547d33a08f7c47dffb68e
delaney <dck5216@psu.edu>
2020-04-24 16:28:38 -0400

    Changed MMSimage.py to return the correct ip for getting images

--- MMSimage.py
+++ MMSimage.py
@@ -22,4 +22,4 @@ def img(customer_id):
     draw.text(xy=(70, 270), text="\n".join(textwrap.wrap(message, width=35)), fill=(0, 0, 0), font=font)
     image.save('Weather-Photos/user{}.jpg'.format(customer_id))
 
-    return 'http://192.241.149.241:8000/user{}.jpg'.format(customer_id)
+    return 'http://weathermywardrobe.com:8000/user{}.jpg'.format(customer_id)
