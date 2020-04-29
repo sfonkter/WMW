769d850ee104b33cb7096d1aabb69d7be179f4e7
delaney <dck5216@psu.edu>
2020-04-29 00:09:37 -0400

    tweaked the startup sequence

--- start.sh
+++ start.sh
@@ -1,9 +1,9 @@
 export FLASK_APP=~/WMW/run.py
 export FLASK_ENV=development
+twilio phone-numbers:update "+18647546178" --sms-url="http://64.227.23.86:5000/sms" &
 workon
-cd ~/WMW
-python scheduler.py &
-flask run --host '0.0.0.0' &
 cd ~/WMW/Weather-Photos
 python -m http.server 8000 &
-twilio phone-numbers:update "+18647546178" --sms-url="http://64.227.23.86:5000/sms" &
+cd ~/WMW
+python scheduler.py &
+flask run --host '0.0.0.0'
