141f8275c37612e520c0eb63be2dd91b6dac81ac
delaney <dck5216@psu.edu>
2020-04-28 23:50:47 -0400

    use flask command line instead

--- run.py
+++ run.py
@@ -215,7 +215,9 @@ def welcome_user(send_function):
     send_function(welcome_text)
 
 
+'''
 if __name__ == "__main__":
     from waitress import serve
     serve(app, host="0.0.0.0", port=5000)
     app.run(debug=True)
+'''
